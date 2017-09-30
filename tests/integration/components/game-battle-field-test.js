cb6b7d57173ab688e4a3ed6c3b6ae0c31eb62ec5
Rastikko <rastikko@gmail.com>
2017-09-30 15:12:11 -0700

    Rename components

--- tests/integration/components/game-battle-field-test.js
+++ tests/integration/components/game-battle-field-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('game-field-card', 'Integration | Component | game field card', {
+moduleForComponent('game-battle-field', 'Integration | Component | game battle field', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{game-field-card}}`);
+  this.render(hbs`{{game-battle-field}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#game-field-card}}
+    {{#game-battle-field}}
       template block text
-    {{/game-field-card}}
+    {{/game-battle-field}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
--- tests/integration/components/game-player-field-test.js
+++ tests/integration/components/game-player-field-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('game-field', 'Integration | Component | game field', {
+moduleForComponent('game-player-field', 'Integration | Component | game player field', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{game-field}}`);
+  this.render(hbs`{{game-player-field}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#game-field}}
+    {{#game-player-field}}
       template block text
-    {{/game-field}}
+    {{/game-player-field}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
