16794dc616b39c8e2471c9d45c1f20c127fe6b29
Rastikko <rastikko@gmail.com>
2017-10-28 12:45:43 -0700

    Enemy and Hero defines

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -5,5 +5,21 @@ export default Ember.Controller.extend({
         return this.get('model.gamePlayers');
     }),
 
+    enemyPlayer: Ember.computed.filter('model.gamePlayers.@each.player', function() {
+        const gamePlayers = this.get('model.gamePlayers');
+
+        return gamePlayers.find((gamePlayer) => {
+            return gamePlayer.get('player.id') !== this.get('session.currentUser.uid');
+        });
+    }),
+
+    heroPlayer: Ember.computed.filter('model.gamePlayers.@each.player', function() {
+        const gamePlayers = this.get('model.gamePlayers');
+
+        return gamePlayers.find((gamePlayer) => {
+            return gamePlayer.get('player.id') === this.get('session.currentUser.uid');
+        });
+    })
+
 
 });
--- tests/integration/components/game/player-permanents-test.js
+++ tests/integration/components/game/player-permanents-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('game/player-field', 'Integration | Component | game/player field', {
+moduleForComponent('game/player-permanents', 'Integration | Component | game/player permanents', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{game/player-field}}`);
+  this.render(hbs`{{game/player-permanents}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#game/player-field}}
+    {{#game/player-permanents}}
       template block text
-    {{/game/player-field}}
+    {{/game/player-permanents}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
